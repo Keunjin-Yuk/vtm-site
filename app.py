dcdf31282e442dcebf08ee423b5f010da1705de6
Jin Yuk <keunjin-yuk@uiowa.edu>
2022-04-04 21:53:51 -0500

    12th commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
@@ -7,14 +7,39 @@ app = Flask(__name__)
 def index():
     return render_template('index.html' , pageTitle='Flask Server Honme page')
 
-@app.route('/jin')
+@app.route('/about')
 def jin():
-    return render_template('jin.html', pageTitle='ALL About Jin')
+    return render_template('about.html', pageTitle='About VTM')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['POST','GET'])
 def estimate():
+    Estimate=''
+    if request.method == 'POST':
+        form=request.form
+        Radius = float(form['Radius'])
+        Height = float(form['Height'])
+        pi=3.14
+        
+        tanktoparea=pi*(Radius*Radius)
 
-    return render_template('estimate.html', pageTitle='Estimation')
+        tanksidearea= 2*(pi*(Radius*Height))
+
+        totalarea= tanktoparea+tanksidearea
+
+        inchtosq= totalarea/144
+
+        materialcost=25
+        totalmaterialcost=materialcost*inchtosq
+
+        laborcost=15
+        totallaborcost=laborcost*inchtosq
+
+        Estimate = totalmaterialcost + totallaborcost
+
+        #return redirect(url_for('index'))
+        return render_template('estimate.html', pageTitle='Estimation',Estimate=Estimate) 
+    #return redirect(url_for('estimate'))
+    return render_template('estimate.html', pageTitle='Estimation',Estimate=Estimate)
 
 if __name__ == '__main__':
     app.run(debug=True)
